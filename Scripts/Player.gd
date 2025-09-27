ec769d994c52361a839c200a8673dfe2d5423474
Elm3tez <mutazquran98@outlook.com>
2025-09-27 11:24:49 +0300

    .

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,13 +5,13 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -20,11 +20,12 @@ func _physics_process(delta: float) -> void:
 		anim.flip_h = false
 		anim.play("Run")
 		velocity.x = direction * SPEED
-	if direction == -1:
+	elif direction == -1:
 		anim.flip_h = true
 		anim.play("Run")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if velocity.y == 0:
+			anim.play("Idle")
 	move_and_slide()
