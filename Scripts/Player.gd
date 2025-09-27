7fadc23ea5d3f5f32a85204659314a07baeb6dfb
Elm3tez <mutazquran98@outlook.com>
2025-09-27 11:47:40 +0300

    .

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,8 +13,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Move_back","Move_front")
 	if direction == 1:
 		anim.flip_h = false
